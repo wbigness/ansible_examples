7b325b490e4a53104097a1a07e5fbd5057cc3964
Bill Bigness <wbigness@gmail.com>
2021-10-19 13:54:13 -0400

    update

--- test_auth.py
+++ test_auth.py
@@ -5,17 +5,17 @@ from getpass import getpass
 
 
 my_parser = argparse.ArgumentParser(description="arg parser hidden password input.")
-my_parser.add_argument('-u',
+my_parser.add_argument('-u', 
                        '--username',
                        required=True,
                        dest='username',
                        help='Username on the device')
 
-my_parser.add_argument('-i',
+my_parser.add_argument('-i', 
                        '--ip-address',
                        required=True,
                        dest='ip',
-                       help='IP address of the device')
+                       help='IP address of the device') 
 
 #define variables from args
 args = my_parser.parse_args()
@@ -36,25 +36,25 @@ ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 print("logging in now")
-ssh_login = ssh.connect(my_ip, port=22,
-                        username=my_user,
-                        password=my_pass,
-                        timeout=60,
+ssh_login = ssh.connect(my_ip, port=22, 
+                        username=my_user, 
+                        password=my_pass, 
+                        timeout=60, 
                         banner_timeout=30)
 
 # starts an interactive session
-channel = ssh.invoke_shell()
+channel = ssh.invoke_shell() 
 # set buffer size via nbytes
 out = channel.recv(9999)
 
 # loop through the commands above, requires concat'ed '\n'
 for command in commands:
     channel.send(command+'\n')
-
+    
 
 while not channel.recv_ready():
     time.sleep(1)
-
+    
 out = channel.recv(9999)
 print(out.decode("ascii"))
 
